53c608c98e3918b375effb2890bac23258403b9f
Cerydra <zhaotianshaohan@outlook.com>
2026-07-01 17:36:59 +0800

    Agent工具绑定

--- backend/src/utils/agent.py
+++ backend/src/utils/agent.py
@@ -1,6 +1,7 @@
 from utils.model import model
 from langchain.agents import create_agent
-from langchain.messages import SystemMessage,HumanMessage,AIMessage
+from utils.tools import get_commend,get_files,excute_command
+from langchain.messages import SystemMessage,HumanMessage,AIMessage  # TODO: 导入了 HumanMessage, AIMessage 但从未使用
 
 '''
 三个Agent
@@ -15,19 +16,20 @@ agent_chat：用户对话agent，负责与用户对话
 agent_search = create_agent(
     model = model,
     system_prompt=SystemMessage(content='你是一个RAG知识库查询AI，你负责查询知识库用以获取相关的知识和终端命令'),
-    # tools=
+    tools=[get_commend]  
 )
 
 # 命令执行agent，负责执行ffmpeg相关命令
+# 需要知道执行命令的传入文件和传出文件位置，对于命令的情况进行反馈
 agent_excute = create_agent(
     model=model,
     system_prompt=SystemMessage(content='你是一个任务执行AI，你负责对文件执行指令'),
-    # tools=
+    tools=[get_files,excute_command]  
 )
 
 # 用户对话agent，负责与用户对话
 agent_chat = create_agent(
     model = model,
     system_prompt=SystemMessage(content='你是一个反馈AI，将结果返回给对话的用户')
-    # tools=
+    # tools=  # TODO: tools= 被注释。如果 chat 需要查询文件列表等能力，也应绑定对应工具
 )
